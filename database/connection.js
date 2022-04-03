6024c34c0aaa60409857e7d987976f0b9ae35d32
Aditya Lavate <84095872+aditya06px@users.noreply.github.com>
2022-04-03 09:56:22 +0530

    Add files via upload

---
+++ database/connection.js
@@ -0,0 +1,10 @@
+import Mongoose from "mongoose";
+
+export default async () => {
+  return Mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: true,
+  });
+};
